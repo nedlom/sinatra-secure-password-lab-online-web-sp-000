8b382e587561f436fe41e70a265c444fd04c165a
imperative-scientist-4600 <nelom3@gmail.com>
2021-05-03 20:17:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
 	      )
 	      
 	  binding.pry  
-	  if user.save
+	  if user.save && username
 	    redirect "/login"
 	  else
 	    redirect "/failure"
